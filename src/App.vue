09b8188f25a17085b04833126825a3e3f974cbc9
SUCCESSSOFTWARE\ha.nguyen <ha.nguyen@successsoftware.global>
2022-06-08 17:27:40 +0700

    'upsource'

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,131 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class='container'>
+    <Header @toggle-add-task="toggleAddTask" title='Task Tracker' :showAddTask="showAddTask" />
+    <div v-if="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks='tasks'></Tasks>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
+import AddTask from './components/AddTask.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Tasks,
+    AddTask
+  },
+  data () {
+    return {
+      tasks: [],
+      showAddTask: false
+    }
+  },
+  methods: {
+    async addTask (task) {
+      const res = await fetch('api/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(task)
+      })
+      const data = await res.json()
+      this.tasks = [...this.tasks, data]
+    },
+    async deleteTask (id) {
+      if (confirm('Are you sure dele?')) {
+        const res = await fetch(`api/tasks/${id}`, {
+          method: 'DELETE'
+        })
+        res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Error deleting task')
+      }
+    },
+    async toggleReminder (id) {
+      const taskToggle = await this.fetchTask(id)
+      const upTask = { ...taskToggle, reminder: !taskToggle.reminder }
+      const res = await fetch(`api/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(upTask)
+      })
+      const data = await res.json()
+      console.log('data', data)
+      this.tasks = this.tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder } : task)
+    },
+    toggleAddTask () {
+      this.showAddTask = !this.showAddTask
+    },
+    async fetchTask () {
+      const res = await fetch('api/tasks')
+      const data = await res.json()
+      return data
+    }
+  },
+  async fetchTask (id) {
+    const res = await fetch(`api/task/${id}`)
+    const data = await res.json()
+    return data
+  },
+  async created () {
+    this.tasks = await this.fetchTask()
   }
+
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Poppins', sans-serif;
+}
+
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.btn:active {
+  transform: scale(0.98);
+}
+
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'https://62431f84b6734894c15b41ad.mockapi.io',
+        changeOrigin: true,
+        logLevel: 'debug',
+        pathRewrite: { '^/api': '/' },
+      },
+    },
+  },
+}
